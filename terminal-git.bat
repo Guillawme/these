9ddcd436352892ba83dcd2c5810b0f5efe950156
Guillaume Gaullier <guillaume.gaullier@gmail.com>
2015-03-03 17:57:41 +0100

    Ajout des scripts batch pour pouvoir utiliser git, emacs et pandoc sous Windows 7 sur mon poste du labo

---
+++ terminal-git.bat
@@ -0,0 +1,7 @@
+﻿REM Ce fichier permet d'ouvrir un terminal directement dans
+REM le dossier "Mes documents", et appelle set-env.bat qui 
+REM définit correctement les variables d'environnement nécessaires 
+REM pour que git trouve son fichier de configuration
+
+cd C:\Users\gg226854\Documents\
+START "git" /D C:\Users\gg226854\Documents\ /B set-env.bat
