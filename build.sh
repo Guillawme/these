f02150e1f17ca53e8ee7a6977c2295089432aad3
Guillaume Gaullier <guillaume.gaullier@gmail.com>
2015-07-17 22:53:04 +0200

    build.sh : commande pour générer le fichier final, see #29

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 
 pandoc -S --filter pandoc-citeproc \
        metadata.yml intro/chap*.md \
+       remerciements.md \
+       citations.md \
        partie-1/intro.md partie-1/resultats.md partie-1/discussion.md \
        partie-2/intro.md partie-2/resultats.md partie-2/discussion.md \
        partie-3/intro.md partie-3/resultats.md partie-3/discussion.md \
+       conclusion-perspectives.md \
        mat-meth/mat-meth.md \
        mat-meth/constructions.md \
        mat-meth/expression-prots.md \
