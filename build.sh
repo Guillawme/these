eb52185f16e238c4a43d768ddda1a385d9899f71
Guillaume Gaullier <guillaume.gaullier@gmail.com>
2015-07-17 10:10:49 +0200

    build.sh : modification mineure

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-pandoc -s -S -N --toc --filter pandoc-citeproc \
+pandoc -S --filter pandoc-citeproc \
        metadata.yml intro/chap*.md \
        partie-1/intro.md partie-1/resultats.md partie-1/discussion.md \
        partie-2/intro.md partie-2/resultats.md partie-2/discussion.md \
        partie-3/intro.md partie-3/resultats.md partie-3/discussion.md \
+       mat-meth/mat-meth.md \
        mat-meth/constructions.md \
        mat-meth/expression-prots.md \
        mat-meth/purification-prots.md \
@@ -22,5 +23,5 @@ pandoc -s -S -N --toc --filter pandoc-citeproc \
        mat-meth/alignements.md \
        annexes/annexes.md \
        references.md \
-       -t html5 -o these-tmp.html
+       -o ~/Desktop/these.docx
 
