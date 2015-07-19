a89a9f7a41133cb81a445d42ea96c1db8ef24570
Guillaume Gaullier <guillaume.gaullier@gmail.com>
2015-07-19 15:01:05 +0200

    build.sh mis à jour avec les nouveaux noms de fichiers

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 pandoc -S --filter pandoc-citeproc \
        metadata.yml \
+       glossaire.md \
        remerciements.md \
        citations.md \
        intro/intro.md intro/chap*.md \
        partie-1/intro.md partie-1/resultats.md partie-1/discussion.md \
        partie-2/intro.md partie-2/resultats.md partie-2/discussion.md \
        partie-3/intro.md partie-3/resultats.md partie-3/discussion.md \
-       conclusion-perspectives.md \
+       conclusion.md \
        mat-meth/mat-meth.md \
        mat-meth/constructions.md \
        mat-meth/expression-prots.md \
